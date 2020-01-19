a507f341c1fc921c3cc08c7b2968ffd65c956dbe
alex.dana@epitech.eu <alex.dana@epitech.eu>
2020-01-19 23:03:38 +0100

    Activated download button

--- src/modules/video/actions/downloadVideo.js
+++ src/modules/video/actions/downloadVideo.js
@@ -2,23 +2,21 @@ import axios from "axios";
 
 import { IS_DOWNLOADING, DOWNLOADED, DOWNLOAD_ERROR } from "../constants";
 
-export default function downloadVideos(dispatch, options = {}) {
+export default function downloadVideo(dispatch, options = {}) {
   dispatch({ type: IS_DOWNLOADING });
 
-  console.log(options);
-  return dispatch({ type: DOWNLOADED });
-
-  // return axios(process.env.REACT_APP_URL_API, {
-  //   url: options.url,
-  //   tagTitle: options.tagTitle,
-  //   tagArtist: options.tagArtist,
-  //   tagAlbum: options.tagAlbum
-  // })
-  //   .then(() => {
-  //     dispatch({ type: DOWNLOADED });
-  //   })
-  //   .catch(err => {
-  //     console.log(err);
-  //     dispatch({ type: DOWNLOAD_ERROR, error: err.message });
-  //   });
+  return axios
+    .post(process.env.REACT_APP_URL_API, {
+      url: options.url,
+      tagTitle: options.tagTitle,
+      tagArtist: options.tagArtist,
+      tagAlbum: options.tagAlbum
+    })
+    .then(() => {
+      dispatch({ type: DOWNLOADED });
+    })
+    .catch(err => {
+      console.log(err);
+      dispatch({ type: DOWNLOAD_ERROR, error: err.message });
+    });
 }
